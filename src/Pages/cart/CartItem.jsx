cc4525ddc7a12b9e66917ac63d0fd8d9d26210e8
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-01-28 19:03:10 +0530

    final

--- src/Pages/cart/CartItem.jsx
+++ src/Pages/cart/CartItem.jsx
@@ -1,34 +1,40 @@
-import React, { useContext } from 'react';
-import { ShopContext } from '../../context/Shop_context';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import './Cartitems.css'
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/Shop_context";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import "./Cartitems.css";
 
-export const CartItem = ({ data }) => { // Destructure data directly in function argument
-    const { id, productName, price, productImage } = data;
-    const {removeToCart } = useContext(ShopContext);
-    const navigate = useNavigate(); // Get the navigate function
+export const CartItem = ({ data }) => {
+  // Destructure data directly in function argument
+  const { id, productName, price, productImage } = data;
+  const { removeToCart } = useContext(ShopContext);
+  const navigate = useNavigate(); // Get the navigate function
 
-    const handleBuy = () => {
-        navigate(`/buy/${id}`); // Navigate to the Buy page
-    };
+  const handleBuy = () => {
+    navigate(`/buy/${id}`); // Navigate to the Buy page
+  };
 
-    return (
-        <div className='cartItem'>
-            <img src={productImage} alt="" />
-            <div className='description'>
-                <p>
-                    <b>{productName}</b>
-                </p>
-                <p><b>{price}</b></p>
-                <div className='countHandler'>
-                    <button className='remove' onClick={() => removeToCart(id)}>REMOVE</button>
-                  
-                    
-                </div>
-            <button className='tobuy' onClick={handleBuy}>Buy</button> {/* Call handleBuy on button click */}
-            </div>
+  return (
+    <div className="cartItem">
+      <img src={productImage} alt="" />
+      <div className="description">
+        <p className="productName">
+          <b>{productName}</b>
+        </p>
+        <p className="price">
+          <b>{price}</b>
+        </p>
+        <div className="countHandler">
+          <button className="remove" onClick={() => removeToCart(id)}>
+            REMOVE
+          </button>
         </div>
-    );
+        <button className="tobuy" onClick={handleBuy}>
+          Buy
+        </button>
+        {/* Call handleBuy on button click */}
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
